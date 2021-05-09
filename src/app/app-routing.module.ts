80b6b4839a0520e6bad3b800f76bff150663a790
Dusan Argalas <argalas.dn@gmail.com>
2021-05-09 16:24:59 +0200

    feat: generated blank components, app routing, lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ContactsBookComponent } from './contacts-book/contacts-book.component';
+import { DetailContactComponent } from './detail-contact/detail-contact.component';
 
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    {
+      path: '',
+      children: [
+    {
+      path: '',
+      component: ContactsBookComponent
+    },
+    {
+      path: 'detail',
+      loadChildren: () => import('./detail-contact/detail-contact.module').then(m => m.DetailContactModule)
+    },
+    {
+      path: 'add',
+      loadChildren: () => import('./add-contact/add-contact.module').then(m => m.AddContactModule)
+    },
+    {
+      path: '**',
+      redirectTo: '',
+      component: ContactsBookComponent
+    }
+  ]
+  }
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { AddContactModule } from './add-contact/add-contact.module';
+import { DetailContactModule } from './detail-contact/detail-contact.module';
+import { ContactsBookModule } from './contacts-book/contacts-book.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AddContactModule,
+    DetailContactModule,
+    ContactsBookModule,
     BrowserModule,
     AppRoutingModule,
     EffectsModule.forRoot([]),
